35c6d9ef6b495c81777bb29a8579d80cadeffa35
BÜLENT ERCAN <34336564+tahaercan2749@users.noreply.github.com>
2022-07-17 21:12:28 +0300

    initial commit 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    protected $primaryKey ="user_id";
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
+        'is_active',
     ];
 
     /**
